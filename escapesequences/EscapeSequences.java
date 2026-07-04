8a2d493834fbd227cfe7db8c9060c82c60bfb570
plernpleng <kittiyapornnedtip@gmail.com>
2026-07-04 18:21:34 +0700

    Add files via upload

---
+++ escapesequences/EscapeSequences.java
@@ -0,0 +1,11 @@
+package escapesequences;
+
+public class EscapeSequences {
+    public static void main(String[] args) {
+        System.out.println("Java \n26");
+        System.out.println("Java \t26");
+        System.out.println("Java 26\b");
+        System.out.println("I\'m a boy.");
+        System.out.println("I\\m a boy.");
+    }
+}
